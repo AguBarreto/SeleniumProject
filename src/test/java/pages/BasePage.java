1e586f5d5831c6c871d0f68f784d4c9e2db3c555
agubarreto <agustinbarreto2022@hotmail.com>
2024-01-19 18:20:21 -0300

    First Commit

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,10 +2,12 @@ package pages;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
@@ -26,5 +28,17 @@ public class BasePage {
 
     public static void navigateTo(String url){
         driver.get(url);
+    }
+
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+    private WebElement Find(String Locator){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Locator)));
+    }
+
+    public void clickElement(String locator){
+        Find(locator).click();
     }
 }
