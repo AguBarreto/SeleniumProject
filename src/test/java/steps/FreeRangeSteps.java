100eca5ca2b7549e7ef23ba474a9597a999c24f1
agubarreto <agustinbarreto2022@hotmail.com>
2024-01-22 11:57:15 -0300

    commit

--- src/test/java/steps/FreeRangeSteps.java
+++ src/test/java/steps/FreeRangeSteps.java
@@ -1,30 +1,36 @@
 package steps;
- 
+
 import io.cucumber.java.en.*;
 import pages.PaginaCursos;
 import pages.PaginaFundamentosTesting;
 import pages.PaginaPrincipal;
- 
+
 public class FreeRangeSteps {
- 
+
     PaginaPrincipal landingPage = new PaginaPrincipal();
     PaginaCursos cursosPage = new PaginaCursos();
     PaginaFundamentosTesting fundamentosPage = new PaginaFundamentosTesting();
- 
+
     @Given("I navigate to www.freerangetesters.com")
     public void iNavigateToFRT() {
         landingPage.navigateToFreeRangeTesters();
     }
- 
+
     @When("I go to {word} using the navigation bar")
     public void navigationBarUse(String section) {
         landingPage.clickOnSectionNavigationBar(section);
     }
- 
+
+    @When("I select Elegir Plan")
+    public void selectElegirPlan() {
+        landingPage.clickOnElegirPlanButton();
+    }
+
     @And("select Introducción al Testing")
     public void navigateToIntro() {
         cursosPage.clickFundamentosTestingLink();
         fundamentosPage.clickIntroduccionTestingLink();
+
     }
- 
+
 }
--- src/test/resources/features/FreeRangeNavegacion.feature
+++ src/test/resources/features/FreeRangeNavegacion.feature
@@ -13,8 +13,12 @@ Feature: Navigation
     #         | Udemy      |
     #         | Mentorías  |
     #         | Newsletter |
- 
-    Scenario: Courses are presented correctly to potential customers
+
+    # Scenario: Courses are presented correctly to potential customers
+    #     Given I navigate to www.freerangetesters.com
+    #     When I go to Cursos using the navigation bar
+    #     And select Introducción al Testing
+
+    Scenario: Users can select a plan when signing up
         Given I navigate to www.freerangetesters.com
-        When I go to Cursos using the navigation bar
-        And select Introducción al Testing
+        When I select Elegir Plan
